62bf37d79d6905c49bf455b8db48725e42c2e5cd
michiharu <michi1diver@gmail.com>
2023-09-18 15:24:34 +0900

    remove output: static

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://michiharu.github.io',
   base: '/my-way',
-  output: 'static',
+  // output: 'static',
   trailingSlash: 'never',
   integrations: [mdx(), sitemap()],
 });
