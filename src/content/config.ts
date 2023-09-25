8d7b155a2134eb9a849d64b80fc8fe7538f7fd96
michiharu <michi1diver@gmail.com>
2023-09-25 09:07:55 +0900

    :construction: フィードバック下書き

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,9 +1,5 @@
 import { defineCollection, z } from 'astro:content';
 
-const urlSchema = z.custom<`/${string}`>((val) => {
-  return /^\//.test(val as string);
-});
-
 const blog = defineCollection({
   // Type-check frontmatter using a schema
   schema: z.object({
@@ -12,7 +8,8 @@ const blog = defineCollection({
     // Transform string to Date object
     pubDate: z.coerce.date(),
     updatedDate: z.coerce.date().optional(),
-    heroImage: urlSchema.optional(),
+    heroImage: z.string().optional(),
+    draft: z.boolean().optional(),
   }),
 });
 
