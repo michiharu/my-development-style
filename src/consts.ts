1bc7faa4bf9e39b7d9cc280bab23e1dac9c436ed
michiharu <michi1diver@gmail.com>
2023-11-03 23:26:17 +0900

    :wrench: update site name and favicon

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = '陽の道を歩く';
-export const SITE_DESCRIPTION = '開発をしながら考えたことを書き綴ります。';
+export const SITE_TITLE = 'My way';
+export const SITE_DESCRIPTION =
+  'I write down what I thought during development work.';
