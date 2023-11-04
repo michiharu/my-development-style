db378a81277d92107698be43ffcd9d60aef88d5a
michiharu <michi1diver@gmail.com>
2023-11-04 10:55:15 +0900

    :wrench: add filename on code block

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import remarkCodeTitles from 'remark-code-titles';
 
 import sitemap from '@astrojs/sitemap';
 
@@ -8,5 +9,6 @@ export default defineConfig({
   site: 'https://michiharu.github.io',
   base: '/my-way',
   trailingSlash: 'always',
+  markdown: { remarkPlugins: [remarkCodeTitles] },
   integrations: [mdx(), sitemap()],
 });
